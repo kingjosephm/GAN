f1232d1880f9587004853f546224bcc7c03f3b3b
Joe <king.joseph.m@gmail.com>
2021-09-03 13:37:12 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+import json
+import tensorflow as tf
+
+class GAN:
+    def __init__(self, config=None):
+        self.config = config
---
+++ utils.py
@@ -0,0 +1,22 @@
+from PIL import Image
+import os
+
+
+def halve_image(img, output_path):
+    height = 256  # Desired final height
+    width = 256  # Desired final width
+    k = 0
+    subdir = {0: '/real/', 1: '/fake/'}
+
+    im = Image.open(img)
+    imgwidth, imgheight = im.size
+    assert (imgwidth == 512)
+    for i in range(0, imgheight, height):
+        for j in range(0, imgwidth, width):
+            box = (j, i, j + width, i + height)
+            a = im.crop(box)
+            path = output_path + subdir[k]
+            os.makedirs(path, exist_ok=True)
+            a.save(path + os.path.split(img)[-1])
+            k += 1
+    return "Done with all images"
