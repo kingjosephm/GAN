3f8a71b5aeb9ffd4ddf3e27f9a02391170ea6942
Joe <king.joseph.m@gmail.com>
2021-09-15 10:06:28 -0400

    fixed path to requirements file in container_driver.sh. Also added TODOs

--- container_driver.sh
+++ container_driver.sh
@@ -1,4 +1,10 @@
-# Install any missing packages from container image
-pip install -r requirements.txt
+#!/bin/bash
 
+# TODO - log file of all output, remove logging code from tf_pix2pix.py
+
+# TODO - create own docker image to obviate this step
+# Install any missing packages from container image
+pip install -r ./scripts/requirements.txt
+
+# TODO - transform below to accept CLI input, removing config file
 python3 ./scripts/tf_pix2pix.py
