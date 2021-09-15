951f85afe65e0a43293e297b356479644fbdbb48
Joe <king.joseph.m@gmail.com>
2021-09-15 09:44:30 -0400

    updated driver.sh to include transfer of additional scripts to GPU cluster

--- driver.sh
+++ driver.sh
@@ -5,7 +5,7 @@ read -p "Welcome, please enter credentials: " credentials
 
 # Move updated script onto server, replace old scripts in ./scripts dir
 echo "Uploading scripts to server, please enter password"
-scp tf_pix2pix.py config.json "$credentials":scripts # also prompts password
+scp tf_pix2pix.py config.json requirements.txt container_driver.sh "$credentials":scripts # also prompts password
 echo "Done uploading scripts. Now SSH into server, which requires your password again"
 
 # SSH into GPU server
