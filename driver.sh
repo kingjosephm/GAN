800ee70b9d6d4e9eb1236349de5b684a2b27cc8d
Joe <king.joseph.m@gmail.com>
2021-09-20 13:43:18 -0400

    updated driver.sh with new file names

--- driver.sh
+++ driver.sh
@@ -5,7 +5,7 @@ read -p "Welcome, please enter credentials: " credentials
 
 # Move updated script onto server, replace old scripts in ./scripts dir
 echo "Uploading scripts to server, please enter password"
-scp tf_pix2pix.py config.json requirements.txt container_driver.sh "$credentials":scripts # also prompts password
+scp pix2pix.py cycle_gan.py base_gan.py utils.py requirements.txt "$credentials":scripts # also prompts password
 echo "Done uploading scripts. Now SSH into server, which requires your password again"
 
 # SSH into GPU server
