c8d66ee5d8da56f1b3adc69cc190bd8600084aae
Joe <king.joseph.m@gmail.com>
2021-09-20 11:17:25 -0400

    added class InstanceNormalization to utils.py for use in cycle_gan script

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import os
+import tensorflow as tf
 
 
 def split_image(img, output_path):
@@ -20,3 +21,29 @@ def split_image(img, output_path):
             a.save(path + os.path.split(img)[-1])
             k += 1
     return "Done with all images"
+
+class InstanceNormalization(tf.keras.layers.Layer):
+  """Instance Normalization Layer (https://arxiv.org/abs/1607.08022)."""
+
+  def __init__(self, epsilon=1e-5):
+    super(InstanceNormalization, self).__init__()
+    self.epsilon = epsilon
+
+  def build(self, input_shape):
+    self.scale = self.add_weight(
+        name='scale',
+        shape=input_shape[-1:],
+        initializer=tf.random_normal_initializer(1., 0.02),
+        trainable=True)
+
+    self.offset = self.add_weight(
+        name='offset',
+        shape=input_shape[-1:],
+        initializer='zeros',
+        trainable=True)
+
+  def call(self, x):
+    mean, variance = tf.nn.moments(x, axes=[1, 2], keepdims=True)
+    inv = tf.math.rsqrt(variance + self.epsilon)
+    normalized = (x - mean) * inv
+    return self.scale * normalized + self.offset
