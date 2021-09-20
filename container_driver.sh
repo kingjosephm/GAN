a1c4b23ba2d158c03afb2ce5ef3b769b6d00cbfc
Joe <king.joseph.m@gmail.com>
2021-09-20 13:43:28 -0400

    removed container_driver.sh

--- container_driver.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# TODO - create own docker image to obviate this step
-# Install any missing packages from container image
-pip install -r ./scripts/requirements.txt
-
-# TODO - transform below to accept CLI input, removing config file
-python3 ./scripts/tf_pix2pix.py
