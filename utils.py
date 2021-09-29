493f7f847ab4c083f62412ef2c2fd62970fcc56a
Joe <king.joseph.m@gmail.com>
2021-09-29 16:58:08 -0400

    updated functions in utils.py to output greyscale images

--- utils.py
+++ utils.py
@@ -33,7 +33,7 @@ class InstanceNormalization(tf.keras.layers.Layer):
     return self.scale * normalized + self.offset
 
 
-def concat_images(path, output_path, resize_width=None, resize_height=None):
+def concat_images(path, output_path, greyscale=True, resize_width=None, resize_height=None):
     '''
     Function to create concatenated visible (left) and thermal (right) image in same PNG for pix2pix model.
     Some poor-quality visible images were also manually deleted, so the function checks if the matching thermal images
@@ -41,6 +41,7 @@ def concat_images(path, output_path, resize_width=None, resize_height=None):
     stripe in all visible images.
     :param path: str, root dir path
     :param output_path: str, output path
+    :param greyscale: bool, whether or not to output greyscale or color images for both thermal and visible images
     :param resize_width: int, optional to resize width
     :param resize_height: int, optional to resize height
     :return: None, outputs to disk
@@ -70,17 +71,23 @@ def concat_images(path, output_path, resize_width=None, resize_height=None):
             th = cv2.resize(th, (resize_width, resize_height), interpolation= cv2.INTER_LINEAR)
             vi = cv2.resize(vi, (resize_width, resize_height), interpolation= cv2.INTER_LINEAR)
 
-        # Concat and output
+        # Concat images
         conc = np.concatenate((vi, th), axis=1)
+
         dir_name = path.split(os.sep)[-2]
+
+        if greyscale:
+            conc = cv2.cvtColor(conc, cv2.COLOR_BGR2GRAY)
+
         cv2.imwrite(os.path.join(output_path, dir_name + '_' + i[9:]), conc)
 
         """
         # Use via:
         for subdir, dirs, files in os.walk(root):
-            merge_images(subdir, output_path, resize_width=640, resize_height=512)
+            concat_images(subdir, output_path, resize_width=640, resize_height=512)
         """
-def load(image_file):
+
+def load(image_file, greyscale=True):
     """
     :param image_file: str, path to image
     :return: tensorflow.python.framework.ops.EagerTensor
@@ -88,27 +95,35 @@ def load(image_file):
     # Read and decode an image file to a uint8 tensor
     image = tf.io.read_file(image_file)
     try:
-        image = tf.image.decode_png(image)
+        if greyscale:
+            image = tf.image.decode_png(image, 1)
+        else:
+            image = tf.image.decode_png(image)
     except:
-        image = tf.image.decode_jpeg(image)
+        if greyscale:
+            image = tf.image.decode_jpeg(image, 1)
+        else:
+            image = tf.image.decode_jpeg(image)
 
     # Cast to float32 tensors
     image = tf.cast(image, tf.float32)
     return image
 
-def split_images(path, resize_width=None, resize_height=None):
+def split_images(path, output_path, greyscale=True, resize_width=None, resize_height=None):
     '''
     :param path: str, path to root
+    :param output_path: str, output path
+    :param greyscale: bool, whether or not to output greyscale or color images for both thermal and visible images
     :param resize_width: int, optional to resize width
     :param resize_height: int, optional to resize height
     :return: None, outputs to disk
     '''
 
-    images = [i for i in os.listdir(path) if ".png" in i or ".jpeg" in i]
+    images = sorted([i for i in os.listdir(path) if ".png" in i or ".jpeg" in i])
 
     for x in images:
 
-        image = load(os.path.join(path, x))
+        image = load(os.path.join(path, x), greyscale)
 
         # Split each image tensor into two tensors:
         w = tf.shape(image)[1]
@@ -121,8 +136,12 @@ def split_images(path, resize_width=None, resize_height=None):
             input_image = tf.image.resize(input_image, [resize_height, resize_width], method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
             real_image = tf.image.resize(real_image, [resize_height, resize_width], method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
 
+        visible_path = os.path.join(output_path, 'visible')
+        os.makedirs(visible_path, exist_ok=True)
+        thermal_path = os.path.join(output_path, 'thermal')
+        os.makedirs(thermal_path, exist_ok=True)
 
-        cv2.imwrite(os.path.join(path, '../separated/visible', x), real_image.numpy())
-        cv2.imwrite(os.path.join(path, '../separated/thermal', x), input_image.numpy())
+        cv2.imwrite(os.path.join(visible_path, x), cv2. cvtColor(real_image.numpy(), cv2.COLOR_RGB2BGR))
+        cv2.imwrite(os.path.join(thermal_path, x), cv2. cvtColor(input_image.numpy(), cv2.COLOR_RGB2BGR))
 
     return "Done with all images"
