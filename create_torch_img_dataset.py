8744e23289dc87b4388ec07411e589b7f149c6fe
Joe <king.joseph.m@gmail.com>
2022-01-13 10:13:06 -0500

    removed `val` directory as no longer use validation set

--- create_torch_img_dataset.py
+++ create_torch_img_dataset.py
@@ -7,7 +7,6 @@ if __name__ == '__main__':
 
     rootDir = '/Users/josephking/Documents/sponsored_projects/MERGEN/data/FLIR_matched_gray_thermal'
     outputDir = '/Users/josephking/Documents/sponsored_projects/MERGEN/data/FLIR_matched_gray_thermal_pytorch'
-    val_share = 0.1
     test_share = 0.001
 
     random.seed(123)
@@ -17,24 +16,20 @@ if __name__ == '__main__':
         shutil.rmtree(outputDir)
         os.mkdir(outputDir)
         os.mkdir(outputDir + '/train')
-        os.mkdir(outputDir + '/val')
         os.mkdir(outputDir + '/test')
 
     files = [i for i in os.listdir(rootDir) if 'png' in i]
 
-    n_val = math.ceil(len(files) * val_share)
     n_test = math.ceil(len(files) * test_share)
-    n_train = len(files) - n_val - n_test
+    n_train = len(files) - n_test
 
-    val_files = random.sample(files, n_val)
-    test_files = random.sample([i for i in files if i not in val_files], n_test)
-    train_files = [i for i in files if i not in val_files and i not in test_files]
+    test_files = random.sample(files, n_test)
+    train_files = [i for i in files if i not in test_files]
 
-    assert (len(val_files) == n_val) and (len(test_files) == n_test) and (len(train_files) == n_train)
+    assert (len(test_files) == n_test) and (len(train_files) == n_train)
 
     all_files = {}
     all_files['train'] = train_files
-    all_files['val'] = val_files
     all_files['test'] = test_files
 
     # Copy and output files
